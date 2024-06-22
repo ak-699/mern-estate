ce7de164cc8b6688d726486342b6b9b309c76cdc
ak-699 <kumar699.abhishek@gmail.com>
2024-06-22 11:57:33 +0530

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js"
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -16,3 +17,5 @@ app.listen(3000, () => {
 
 // jkooZ4H88Md5Hwv3 - password for mongodb atlas database
 // "mongodb+srv://kumar699abhishek:jkooZ4H88Md5Hwv3@mern-estate.t2ms6ip.mongodb.net/" - connection string"
+
+app.use("/api/user", userRouter);
