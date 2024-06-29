31590fc58bbb5181cd3766c457317927ade0331f
ak-699 <kumar699.abhishek@gmail.com>
2024-06-29 16:41:28 +0530

    create signin api route

--- api/index.js
+++ api/index.js
@@ -22,6 +22,12 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+
+app.get("/test", (req, res) => {
+    console.log("GET /test route hit")
+    res.send("ok")
+})
+
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal Server Error";
